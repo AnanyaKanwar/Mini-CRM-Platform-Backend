78992b2f55d3e3c15a1e7ac85e2b023659337ea8
AnanyaKanwar <kanwarananya93@gmail.com>
2025-05-28 23:06:41 +0530

    Describe your changes here

--- server.js
+++ server.js
@@ -121,9 +121,9 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-    console.log(`app is running on port: ${port}`);
-})
+// app.listen(port, () => {
+//     console.log(`app is running on port: ${port}`);
+// })
 
 
 module.exports = app;
