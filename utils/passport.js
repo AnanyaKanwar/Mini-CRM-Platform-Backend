8efeb1428f36b1eb701d75c0f949693f2f2d6073
AnanyaKanwar <kanwarananya93@gmail.com>
2025-06-02 23:47:00 +0530

    Describe your changes here

--- utils/passport.js
+++ utils/passport.js
@@ -5,7 +5,8 @@ const User = require('../models/User');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "https://minicrmbackend.vercel.app/api/auth/google/callback"
+  callbackURL: "https://minicrmbackend.vercel.app/api/auth/google/callback",
+  passReqToCallback: true
 },
 async (accessToken, refreshToken, profile, done) => {
   let user = await User.findOne({ googleId: profile.id });
